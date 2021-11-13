b43cc826d954bb96f6b6add38f44b2f5363f3e37
Flaviojcf <fjcf@poli.br>
2021-11-12 21:47:36 -0300

    second commit

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -1,16 +1,21 @@
+import { useEffect } from "react";
+import { api } from "../../services/api";
 import { Container } from "./style";
 
-export function Table () {
+export function Table() {
+    useEffect(() => {
+        api.get('transactions').then(response => console.log(response.data))
+    }, []);
     return (
         <Container>
             <table>
                 <thead>
-                  <tr>
-                     <th>Título</th>
-                     <th>Valor</th>
-                     <th>Categoria</th>
-                     <th>Data</th>
-                  </tr>
+                    <tr>
+                        <th>Título</th>
+                        <th>Valor</th>
+                        <th>Categoria</th>
+                        <th>Data</th>
+                    </tr>
                 </thead>
                 <tbody>
                     <tr>
@@ -25,8 +30,8 @@ export function Table () {
                         <td>Casa</td>
                         <td>12/11/2021</td>
                     </tr>
-                    
-                    
+
+
                 </tbody>
             </table>
         </Container>
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -8,7 +8,7 @@ export const GlobalStyle = createGlobalStyle`
     --blue:#5429CC;
     --blue-light:#6933FF;
     --text-title:#363F5F;
-    -text-body:#969CB3;
+    --text-body:#969CB3;
     --shape:#FFFFFF;
 }
 
@@ -51,5 +51,30 @@ button {
     opacity: 0.6;
     cursor: not-allowed;
 }
+
+
+.react-modal-overlay {
+    background: rgba(0, 0, 0, 0.5);
+    position: fixed;
+    top:0;
+    bottom:0;
+    right:0;
+    left:0;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    
+}
+
+.react-modal-content {
+    width: 100%;
+    max-width: 576px;
+    background: var(--background);
+    padding: 3rem;
+    position: relative;
+    border-radius: 0.25rem;
+}
+
 
 `
